5128e0b8363e2bc8054bd1ba06d8efff3fddeffa
Maxim Petrov <maximpetrov@yahoo.com>
2016-08-11 20:06:14 +0300

    test

--- open_edx_api_extension_cms/views.py
+++ open_edx_api_extension_cms/views.py
@@ -1,10 +1,19 @@
 import logging
 from contentstore.views.course import _create_or_rerun_course
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.csrf import csrf_exempt
+from openedx.core.lib.api.permissions import ApiKeyHeaderPermission
+from student.models import CourseAccessRole
+from student.roles import CourseCreatorRole
 
 log = logging.getLogger(__name__)
 
 
-@api_view
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([ApiKeyHeaderPermission])
 def create_or_rerun_course(request):
+    course_creators = CourseAccessRole.objects.filter(role=CourseCreatorRole.ROLE)
+    if course_creators.exists():
+        request.user = course_creators[0].user
     return _create_or_rerun_course(request)
