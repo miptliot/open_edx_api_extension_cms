dbdaad85b6a3d94ce0f5749485aa6873b335f0e4
Maxim Petrov <maximpetrov@yahoo.com>
2016-08-11 21:38:20 +0300

    mmmm

--- open_edx_api_extension_cms/views.py
+++ open_edx_api_extension_cms/views.py
@@ -2,6 +2,8 @@ import logging
 from contentstore.views.course import _create_or_rerun_course
 from django.core.exceptions import PermissionDenied
 import json
+
+from opaque_keys.edx.keys import CourseKey
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
 from openedx.core.lib.api.permissions import ApiKeyHeaderPermission
@@ -25,5 +27,5 @@ def create_or_rerun_course(request):
     response = _create_or_rerun_course(request)
     if response.status_code >= 400:
         return response
-    course_key = json.loads(response.content).get("course_key")
+    course_key = CourseKey.from_string(json.loads(response.content).get("course_key"))
     CourseDetails.update_from_json(course_key, request.json, global_stuff)
