f1ab4bfdb24562205479cb6968f59727dd8c791b
Maxim Petrov <maximpetrov@yahoo.com>
2016-08-11 21:19:09 +0300

    first atempt for course details

--- open_edx_api_extension_cms/urls.py
+++ open_edx_api_extension_cms/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from django.conf import settings
 
 from open_edx_api_extension_cms import views
 
 
 urlpatterns = [
-    url(r'^create_or_rerun_course/$', views.create_or_rerun_course),
+    url(r'^course/$', views.create_or_rerun_course),
 ]
--- open_edx_api_extension_cms/views.py
+++ open_edx_api_extension_cms/views.py
@@ -1,9 +1,11 @@
 import logging
 from contentstore.views.course import _create_or_rerun_course
+from django.core.exceptions import PermissionDenied
 from rest_framework.decorators import api_view, permission_classes
 from django.views.decorators.csrf import csrf_exempt
 from openedx.core.lib.api.permissions import ApiKeyHeaderPermission
 from django.contrib.auth import get_user_model
+from openedx.core.djangoapps.models.course_details import CourseDetails
 
 log = logging.getLogger(__name__)
 
@@ -14,7 +16,13 @@ User = get_user_model()
 @api_view(['POST'])
 @permission_classes([ApiKeyHeaderPermission])
 def create_or_rerun_course(request):
-    global_stuff = User.objects.filter(is_staff=True)
-    if global_stuff.exists():
-        request.user = global_stuff[0]
-    return _create_or_rerun_course(request)
+    global_stuff = User.objects.filter(is_staff=True).first()
+    if global_stuff is not None:
+        request.user = global_stuff
+    else:
+        raise PermissionDenied()
+    response = _create_or_rerun_course(request)
+    if not response.status_code.ok():
+        return response
+    course_key = response.json.get("course_key")
+    CourseDetails.update_from_json(course_key, request.json, global_stuff)
